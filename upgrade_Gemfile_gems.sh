4a5f9c5e9836c2b16627788f0552ecd19f0f79d9
Jacob Hoopes <jake.hoopes@gmail.com>
2026-02-28 10:48:02 -0700

    Fix rdoc to align with bundled ruby rdoc, and update gemspec to reflect lowest supported version of Ruby / updated licesning

--- upgrade_Gemfile_gems.sh
+++ upgrade_Gemfile_gems.sh
@@ -7,6 +7,8 @@ cat Gemfile | awk '{print $2}' | grep -E "^'.+$" | grep -v -e rubygems.org | whi
   echo "${this_gem} => $latest_version"
   if [[ $this_gem == 'rdoc' ]]; then
     echo 'rdoc is now bundled with Ruby. Ignorning version checks in Gemfile to avoid version mismatch issues.'
+    latest_version=`gem list rdoc | sed 's/[(|)]//g' | sed 's/,/ /g' | awk '{print $2}'`
+    sed -i "s/^gem '${this_gem}'.*$/gem '${this_gem}', '${latest_version}'/g" Gemfile
   elif [[ $this_gem == 'bundler' || $this_gem == 'bundler-audit' ]]; then
     latest_version=`gem search -r $this_gem | grep -E "^${this_gem}\s.+$" | awk '{print $2}' | sed 's/(//g' | sed 's/)//g' | sed 's/,//g'`
     sed -i "s/^gem '${this_gem}'.*$/gem '${this_gem}', '>=${latest_version}'/g" Gemfile
